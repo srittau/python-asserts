b1b5a3ccb678836cb3466567b89c96adf7350bd2
Sebastian Rittau <srittau@rittau.biz>
2014-02-21 14:51:05 +0100

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+
+setup(
+    name="asserts",
+    version="0.1",
+    description="Rich Assertions",
+    author="Sebastian Rittau",
+    author_email="srittau@rittau.biz",
+    url="https://github.com/srittau/python-asserts",
+    py_modules=["asserts", "test_asserts"],
+)
