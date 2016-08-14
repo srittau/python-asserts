49656fb017b06ea4af86ffcc3671a11a1ff65591
Sebastian Rittau <srittau@rittau.biz>
2016-08-14 16:15:16 +0200

    change description in setup.py to match that of README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name="asserts",
     version="0.6",
-    description="Rich Assertions",
+    description="Stand-alone Assertions",
     author="Sebastian Rittau",
     author_email="srittau@rittau.biz",
     url="https://github.com/srittau/python-asserts",
