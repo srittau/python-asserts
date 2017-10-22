0b9343315dc04481f3bb589599d909162c381511
Sebastian Rittau <srittau@rittau.biz>
2017-10-22 07:35:14 +0200

    add more metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
+import os
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="asserts",
     version="0.7.0",
     description="Stand-alone Assertions",
+    long_description=read("README.rst"),
     author="Sebastian Rittau",
     author_email="srittau@rittau.biz",
     url="https://github.com/srittau/python-asserts",
     py_modules=["asserts", "test_asserts"],
     license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+    ],
 )
