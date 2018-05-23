154156c5f0821cfa72ee45183cef810bcd1b5ddf
Sebastian Rittau <srittau@rittau.biz>
2018-05-23 20:15:57 +0200

    Bump version to 0.8.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="asserts",
-    version="0.8.2",
+    version="0.8.3",
     description="Stand-alone Assertions",
     long_description=read("README.rst"),
     author="Sebastian Rittau",
